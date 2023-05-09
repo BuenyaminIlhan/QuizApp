45667e32ef9f7489d3683a31c6ca31368234503c
Bünyamin Ilhan <ilhan.buenyain@gmail..com>
2023-05-09 13:31:48 +0200

    add Json

---
+++ script.js
@@ -0,0 +1,81 @@
+let questions = [{
+    'question': 'Welche der folgenden Programmiersprachen ist eine objektorientierte Programmiersprache?',
+    'answer1': 'A) Python',
+    'answer2': 'B) HTML',
+    'answer3': 'C) CSS',
+    'answer4': 'D) SQL',
+    'right-answer': 'A) Python'
+},
+{
+    'question': 'Welche der folgenden Aussagen über JavaScript ist korrekt?',
+    'answer1': 'a) JavaScript ist eine objektorientierte Programmiersprache.',
+    'answer2': 'B) JavaScript wird nur für die Entwicklung von serverseitigen Anwendungen verwendet.',
+    'answer3': 'C) JavaScript wird von allen modernen Browsern unterstützt.',
+    'answer4': 'D) JavaScript kann nur für die Erstellung von statischen Webseiten verwendet werden.',
+    'right-answer': 'C) JavaScript wird von allen modernen Browsern unterstützt.'
+},
+{
+    'question': 'Was ist der Unterschied zwischen "undefined" und "null" in JavaScript?',
+    'answer1': 'A) "undefined" bedeutet, dass eine Variable deklariert wurde, aber keinen Wert hat, während "null" explizit einem Objekt als Wert zugewiesen wurde.',
+    'answer2': 'B) "undefined" bedeutet, dass eine Variable nicht deklariert wurde, während "null" explizit einem Objekt als Wert zugewiesen wurde.',
+    'answer3': 'C) "undefined" und "null" bedeuten dasselbe in JavaScript und können synonym verwendet werden.',
+    'answer4': 'D) "undefined" und "null" sind beide Fehlermeldungen, die auf fehlerhaften Code hinweisen.',
+    'right-answer': 'A) "undefined" bedeutet, dass eine Variable deklariert wurde, aber keinen Wert hat, während "null" explizit einem Objekt als Wert zugewiesen wurde.'
+},
+{
+    'question': 'Welche der folgenden Methoden wird verwendet, um eine Schleife in JavaScript zu beenden?',
+    'answer1': 'A) break;',
+    'answer2': 'B) continue;',
+    'answer3': 'C) return;',
+    'answer4': 'D) exit;',
+    'right-answer': 'A) break;'
+},
+{
+    'question': 'Was ist der Unterschied zwischen == und === Operatoren in JavaScript?',
+    'answer1': 'A) Es gibt keinen Unterschied zwischen den beiden Operatoren.',
+    'answer2': 'B) Der == Operator vergleicht den Wert und den Datentyp der Variablen, während der === Operator nur den Wert vergleicht',
+    'answer3': 'C) Der === Operator vergleicht den Wert und den Datentyp der Variablen, während der == Operator nur den Wert vergleicht.',
+    'answer4': 'D) Der == Operator ist veraltet und sollte nicht mehr verwendet werden.',
+    'right-answer': 'C) Der === Operator vergleicht den Wert und den Datentyp der Variablen, während der == Operator nur den Wert vergleicht.'
+},
+{
+    'question': 'Welches der folgenden Konstrukte in JavaScript wird verwendet, um asynchrone Operationen auszuführen und auf das Ergebnis zu warten?',
+    'answer1': 'A) Promises',
+    'answer2': 'B) Callbacks',
+    'answer3': 'C) Arrays',
+    'answer4': 'D) Loops',
+    'right-answer': 'A) Promises'
+},
+{
+    'question': 'Welche der folgenden Methoden wird verwendet, um ein Element in JavaScript zu entfernen?',
+    'answer1': 'A) element.remove()',
+    'answer2': 'B) element.hide()',
+    'answer3': 'C) element.delete()',
+    'answer4': 'D) element.detach()',
+    'right-answer': 'A) element.remove()'
+},
+{
+    'question': 'Welche der folgenden Methoden wird verwendet, um eine JavaScript-Funktion in regelmäßigen Abständen auszuführen?',
+    'answer1': 'A) setTimeout()',
+    'answer2': 'B) setInterval()',
+    'answer3': 'C) setImmediate()',
+    'answer4': 'D) requestAnimationFrame()',
+    'right-answer': 'B) setInterval()'
+},
+{
+    'question': 'Was ist der Unterschied zwischen let, const und var in JavaScript?',
+    'answer1': 'A) Es gibt keinen Unterschied zwischen let, const und var.',
+    'answer2': 'B) var ist veraltet und sollte nicht mehr verwendet werden.',
+    'answer3': 'C) let und const sind blockscoped, während var functionscoped ist.',
+    'answer4': 'D) const wird verwendet, um Konstanten zu deklarieren, während let und var für Variablen verwendet werden.',
+    'right-answer': 'C) let und const sind blockscoped, während var functionscoped ist.'
+},
+{
+    'question': 'Welches der folgenden Ereignisse wird ausgelöst, wenn ein Benutzer einen Mausklick auf ein Element ausführt und ihn dann loslässt?',
+    'answer1': 'A) mouseover',
+    'answer2': 'B) mouseout',
+    'answer3': 'C) mouseclick',
+    'answer4': 'D) mouseup',
+    'right-answer': 'D) mouseup'
+},
+];
